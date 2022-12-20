4ff36751e9f4f1723755a44b29d4fbd2419baeca
bwni <bokpoot@hotmail.com>
2022-12-20 15:02:20 +0100

    Main added to the dir and tested some ST7789V display controller github tutorial

---
+++ main.py
@@ -0,0 +1,22 @@
+# Made by: Bram Nijhoff
+# Date: 2/12/2022
+
+"""
+Welcome to the Automate Indoor Growing Box (AIGB) control program.
+The main file is used to control incoming signals of sensors 
+and buttons, processing the data by visualising it on the display, 
+and sending outgoing signals to the motors and LED-strips.
+
+
+Goal: Automate the process of growing plants.
+
+Result: 	A fully growed herb or lettuce plant, or germinated sprouds 
+			of plants like vegetables and fruits.
+"""
+
+"""
+How-to:
+Follow the guide on the following website:
+https://www.waveshare.com/wiki/2inch_LCD_Module
+
+"""
